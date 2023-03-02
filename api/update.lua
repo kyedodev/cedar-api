4b99522494f660cac340e926b34b33f17a2b7f86
Kye Cedar <91228490+kyedodev@users.noreply.github.com>
2023-03-02 23:50:24 +0000

    updated to install multiple files and use github instead.

--- api/update.lua
+++ api/update.lua
@@ -1,29 +1,65 @@
 -- pastebin get 1ivnjzgi update
 
 local cwd      = fs.getDir(shell.getRunningProgram())
-local filename = "cedar"
-local file     = cwd .. "/" .. filename
+local file     = cwd .. "/"
 
 local old_version
 
+local api = {
+  {
+    filename = "cedar.lua",
+    url      = "https://raw.githubusercontent.com/kyedodev/cedar-api/main/api/cedar.lua"
+  },
+  {
+    filename = "update.lua",
+    url      = "https://raw.githubusercontent.com/kyedodev/cedar-api/main/api/update.lua"
+  }
+}
 
 
+
+-- fetches file from internet.
+function downloadFile(url, filename)
+  if not http.checkURL(url) then
+    return error("Invalid download url: " .. url .. "\nCheck formatting or the HTTP API configs in ComputerCraft.cfg")
+  end
+
+  local res = http.get(url)
+
+  if res then
+    local file = fs.open(cwd .. filename, "w")
+    file.write(res.readAll())
+    file.close()
+    print("Downloaded " .. filename .. ".")
+  else
+    return error("Could not download file '" .. filename .. "' from " .. url .. "\n\n" .. "Could not finish installation.\nCheck internet connection or if the HTTP API is enabled in ComputerCraft.cfg")
+  end
+end
+
+
+
+-- check if api is loaded, store old version number in var.
 if not (cedar == nil) then
   old_version = cedar.version
   print("Cedar API found, v" .. old_version)
 end
 
 
--- delete file if exists.
-if fs.exists(file) then
-  fs.delete(file)
+
+for i in pairs(api) do
+  -- deleting all files if they exist.
+  local filename, url = api[i].filename, api[i].url
+  if fs.exists(filename) then
+    fs.delete(filename)
+  end
+
+  -- replace file with new, improved one from download :3
+  downloadFile(url, filename)
 end
 
 
--- installing.
-shell.run("pastebin","get","tcqanvzb",filename)
 
-os.loadAPI(file)
+os.loadAPI(file .. "cedar.lua")
 
 local new_version = cedar.version
 
